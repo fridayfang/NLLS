78e6b11d2377d9dad60708d0f3747faf8a74cbe2
fridayfang <fang_shuai@pku.edu.cn>
2021-01-18 22:24:07 +0800

    implement GN

--- src/Gauss_Newton.hpp
+++ src/Gauss_Newton.hpp
@@ -3,6 +3,7 @@
 
 #include <DataType.hpp>
 #include <Eigen/LU>
+#include <iostream>
 //针对平面运动的非线性最小二乘
 // GN 实现的是Gauss-Newton 法
 using namespace std;
@@ -40,7 +41,8 @@ class GN{
 
         void computeJacoAndError(const Vertex& vi, const Vertex& vj, const Edge& eij,Vector3d& error_ij, Matrix3d& Aij, Matrix3d& Bij){
             
-            error_ij(2) = vj.estimate(2)-vi.estimate(2)-eij.measure(2);
+            error_ij(2) =  vj.estimate(2)-vi.estimate(2)-eij.measure(2);
+            bound(error_ij(2));
             
             Matrix3d mi = v2t(vi.estimate);
             Matrix3d mj = v2t(vj.estimate);
@@ -63,8 +65,10 @@ class GN{
         VectorXd linearizeAndSolve(){
             MatrixXd H(verx_ptr->size()*3, verx_ptr->size()*3);
             VectorXd b(verx_ptr->size()*3);
+            H.setZero();
+            b.setZero();
 
-            H.block(0,0,3,3) = Matrix3d::Identity();//fix firstb frame
+            H.block(0,0,3,3) += Matrix3d::Identity();//fix firstb frame
 
             for(int i = 0; i<edges_ptr->size();i++){
                 Edge eij =  edges_ptr->at(i);
@@ -86,18 +90,74 @@ class GN{
                 H.block(3*eij.xj,3*eij.xi,3,3) += Hji;
                 H.block(3*eij.xj,3*eij.xj,3,3) +=Hjj;
                 
-                b.block(3*eij.xi,0,3,1) += err_ij.transpose()*eij.info*Aij;
-                b.block(3*eij.xj,0,3,1) += err_ij.transpose()*eij.info*Bij;
+                // b.segment(3*eij.xi,3) += ( err_ij.transpose()*eij.info*Aij ).transpose();
+                // b.segment(3*eij.xj,3) += ( err_ij.transpose()*eij.info*Bij ).transpose();
 
+                Vector3d bi,bj;
+                bi = ( err_ij.transpose()*eij.info*Aij ).transpose();
+                bj = (err_ij.transpose()*eij.info*Bij).transpose();
+                    
+                b(3*eij.xi)+=bi(0);
+                b(3*eij.xi+1)+=bi(1);
+                b(3*eij.xi+2)+=bi(2);
+
+                b(3*eij.xj)+=bj(0);
+                b(3*eij.xj+1)+=bj(1);
+                b(3*eij.xj+2)+=bj(2);
+
+                if(i==2){
+                    cout<<"Aij"<<Aij<<endl;
+                    cout<<"Bij"<<Bij<<endl;
+                    cout<<"err_ij"<<err_ij<<endl;
+                    cout<<"H"<<H<<endl;
+                    cout<<"b"<<b<<endl;
+                }
+            // cout<<"H:\n"<<H<<endl;
+            // cout<<"b:"<<b<<endl;
 
 
             }
             // get H,b 
             // return dx
+            cout<<"H:\n"<<H<<endl;
+            cout<<"b:"<<b<<endl;
+
             VectorXd dx = -H.lu().solve(b);
             return dx;
         }
 
+        void bound(double& theta){
+            while(theta>M_PI){
+                theta-=M_PI*2;
+                
+            }
+            while(theta<=-M_PI){
+                theta+=M_PI*2;
+            }
+        }
+        void solve(){
+            VectorXd dx = linearizeAndSolve();
+            cout<<"dx:\n"<<dx<<endl;
+            int iters = 0;
+            while(iters<100 && abs( dx.mean() )>0.01)
+            {
+                for(int i=0;i<verx_ptr->size(); i++){
+                    verx_ptr->at(i).estimate += dx.segment(i*3,3);
+                    
+                    bound( verx_ptr->at(i).estimate(2) );
+                }
+                dx = linearizeAndSolve();
+                iters++;
+            }
+            cout<<"after "<<iters<<" end"<<endl;
+            cout<<"dx.sum() = "<<dx.sum()<<endl;
+
+        }
+        void printResult(){
+            for(int i=0;i<verx_ptr->size();i++){
+               cout<<verx_ptr->at(i).estimate(0)<<","<<verx_ptr->at(i).estimate(1)<<","<<verx_ptr->at(i).estimate(2)<<endl;
+            }
+        }
 
 };
 
